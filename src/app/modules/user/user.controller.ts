df5d0601db5bf34fe5a94a3cbe741c4b4beb6493
razumolla <rajuahmed9344@gmail.com>
2023-05-22 22:28:57 +0600

    get user by id

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from "express";
-import { createUserToDB, getUsersFromDB } from "./user.service";
+import {
+  createUserToDB,
+  getUserByIdFromDB,
+  getUsersFromDB,
+} from "./user.service";
 
 export const createUser = async (
   req: Request,
@@ -28,8 +32,16 @@ export const getUsers = async (
   });
 };
 
-/**
- * Pattern
- * ---------
- * Route -> controller -> service
- */
+export const getUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+  const user = await getUserByIdFromDB(id);
+
+  res.status(200).json({
+    status: "success",
+    data: user,
+  });
+};
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -11,3 +11,10 @@ export const getUsersFromDB = async (): Promise<IUser[]> => {
   const users = await User.find();
   return users;
 };
+
+export const getUserByIdFromDB = async (
+  payload: string
+): Promise<IUser | null> => {
+  const user = await User.findOne({ id: payload }, { name: 1, contactNo: 1 });
+  return user;
+};
